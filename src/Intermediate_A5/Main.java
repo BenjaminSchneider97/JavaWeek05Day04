079a1823c38994616a125f12a26af9d8345d003a
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-06-06 13:15:48 +0200

    add file for intermediate a5

---
+++ src/Intermediate_A5/Main.java
@@ -0,0 +1,19 @@
+package Intermediate_A5;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        /*
+        FileWriter fileWriter = new FileWriter("./src/Intermediate_A5/hello.txt", true);
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+        printWriter.println("New line?");
+        printWriter.close();
+         */
+    }
+}
