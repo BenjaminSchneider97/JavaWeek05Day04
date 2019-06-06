852469111aa15e99d2314697e7911cd292a69de9
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-06-06 09:37:56 +0200

    add test.txt and check for its existance

--- src/Basic_A0/Main.java
+++ src/Basic_A0/Main.java
@@ -5,13 +5,16 @@ import java.nio.file.Files;
 import java.io.IOException;
 
 public class Main {
+    /*
     private static void usage() {
         System.err.println("java CreateOrDelete <file>");
         System.err.println(" The <file> argument is required.");
         System.exit(1);
     }
-
+    */
     public static void main(String[] args) {
+
+        /*
         if (args.length != 1)
             usage();
 
@@ -26,5 +29,22 @@ public class Main {
             System.exit(1);
         }
     }
-}
 
+     */
+
+        Path path = Paths.get("./src/Basic_A0/test.txt");
+        System.out.println("path = " + path);
+        System.out.println(" exists = " + Files.exists(path));
+        System.out.println(" readable = " + Files.isReadable(path));
+        System.out.println(" writeable = " + Files.isWritable(path));
+        System.out.println(" executeable = " + Files.isExecutable(path));
+
+        path = Paths.get("/this_file_doesn 't_exist");
+
+        System.out.println("path = " + path);
+        System.out.println(" exists = " + Files.exists(path));
+        System.out.println(" readable = " + Files.isReadable(path));
+        System.out.println(" writeable = " + Files.isWritable(path));
+        System.out.println(" executeable = " + Files.isExecutable(path));
+    }
+}
