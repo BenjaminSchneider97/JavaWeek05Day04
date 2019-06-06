79ab5f45c68a736f43fd95493f615955ffe08d94
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-06-06 11:48:42 +0200

    add intermediate a4

---
+++ src/Basic_A4/Main.java
@@ -0,0 +1,32 @@
+package Basic_A4;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class Main {
+
+    private static void usage(){
+        System.err.println("Error you suck");
+        System.exit(1);
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            usage();
+        }
+
+        Path inputFile = Paths.get(args[0]);
+        Path outputFile = Paths.get(args[1]);
+
+        try {
+            byte[] contents = Files.readAllBytes(inputFile);
+            Files.write(outputFile, contents, StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            System.err.println("Error: " + e);
+            System.exit(1);
+        }
+    }
+}
