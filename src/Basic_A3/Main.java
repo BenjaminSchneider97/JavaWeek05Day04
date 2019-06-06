7a41cd330c8a514a898843a03c26a4fd0427ad91
judith <judithmarialang@gmail.com>
2019-06-06 11:33:11 +0200

    adds Basic A3

--- src/Basic_A3/Main.java
+++ src/Basic_A3/Main.java
@@ -1,4 +1,35 @@
 package Basic_A3;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.io.IOException;
+import java.nio.file.StandardOpenOption;
+import java.rmi.ServerError;
+
+
+
 
 public class Main {
+
+    private static void usage(){
+        System.err.println("Java CP1 <input file> <output file>");
+        System.exit(1);
+    }
+
+    public static void main(String[] args) {
+        if(args.length!= 2) {
+            usage();
+        }
+        Path inputFile = Paths.get(args[0]);
+        Path outputFile = Paths.get(args[1]);
+
+        try{
+            byte[]contents = Files.readAllBytes(inputFile);
+            Files.write(outputFile, contents,
+                    StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            System.err.println("ERROR: "+e);
+            System.exit(1);
+        }
+    }
 }
