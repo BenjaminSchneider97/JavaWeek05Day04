1b576cc77e7665a13c1985fd3bc194aafcbb0a89
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-06-06 14:00:25 +0200

    add second method

--- src/Advanced_A7/Main.java
+++ src/Advanced_A7/Main.java
@@ -1,4 +1,25 @@
 package Advanced_A7;
 
+import java.io.File;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        File file = new File("./src/Advanced_A6/lorem.txt");
+
+        long size = file.length();
+
+        System.out.println("The file has: " + size + " bytes!");
+        System.out.println("The file has: " + calcKiloBytes(file) + " kilobytes!");
+        System.out.println("The file has: " + calcMegaBytes(file) + " megabytes!");
+    }
+
+    private static double calcKiloBytes(File file){
+        return (double) file.length() / 1024;
+    }
+
+    private static double calcMegaBytes(File file){
+        return (double) file.length() / (1024*1024);
+    }
 }
